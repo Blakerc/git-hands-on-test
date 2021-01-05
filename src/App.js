ac7755469bd70776d2c9fc90df5dc08961b3d4c3
Ruairi Blake <Rblake0601@gmail.com>
2021-01-05 12:29:17 -0500

    changed some text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this is the updated text in change-link branch
         </a>
       </header>
     </div>
